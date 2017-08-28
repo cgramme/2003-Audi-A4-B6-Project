2916e8bd1ad7ad2faae86ac2206da20454c9ccd6
cgramme <cgramme36@gmail.com>
2017-08-28 19:49:33 -0400

    Add files via upload

--- script/intercooler.js
+++ script/intercooler.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
 	$('.intercooler-list li').on('click', function(){
 
 		var buttonText = $(this).find('p').text().toLowerCase();
+		if(buttonText === "map diy"){buttonText = "mapDiy";}
 
 		$.ajax({url: "../html/intercoolerText/"+buttonText+".html", success: function(result){
-            $(".main-text").html(result);
+            $(".main-text").addClass('fade-out').delay(500).queue(function(next){
+            $(".main-text").html(result).removeClass('fade-out').addClass('fade-in');
+            next();
+        });
         }});
 
 		//$(this).addClass('fade-in');
